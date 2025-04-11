8796a30b891e0a62abdeb336e385e4ac6bb2f594
Prog <progsolos@gmail.com>
2025-04-11 15:38:00 +0530

    framer-stagger-animation

---
+++ framer-stagger-animation/src/App.tsx
@@ -0,0 +1,13 @@
+import AnimatedGallery from "./components/AnimatedGallery"
+import StaggerAnimations from "./components/StaggerAnimations"
+
+const App = () => {
+  return (
+    <div>
+      {/* <StaggerAnimations /> */}
+      <AnimatedGallery />
+    </div>
+  )
+}
+
+export default App
