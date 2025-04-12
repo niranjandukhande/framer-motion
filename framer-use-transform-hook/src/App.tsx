c3ca7178346bc35cca06a19cb3160a77d1364362
Prog <progsolos@gmail.com>
2025-04-12 17:38:48 +0530

    framer useTransform hook

---
+++ framer-use-transform-hook/src/App.tsx
@@ -0,0 +1,11 @@
+import DraggableBox from "./components/DraggableBox"
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center bg-black h-screen">
+      <DraggableBox />
+    </div>
+  )
+}
+
+export default App
